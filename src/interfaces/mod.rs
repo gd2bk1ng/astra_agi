e3e4603d6a9b3bffb8f3993a87f4f419eeb6c49e
GD2BK1NG <147461464+gd2bk1ng@users.noreply.github.com>
2025-12-24 04:45:02 +0000

    Update mod.rs
    External interfaces and integrations

--- src/interfaces/mod.rs
+++ src/interfaces/mod.rs
@@ -1,1 +1,17 @@
-/interfaces/mod.rs // External interfaces and integrations
+// =============================================================================
+//  Astra AGI - External Interfaces Integrations (AEII)
+//  File: mod.rs
+//
+//  Description: External interfaces and integrations
+//
+//  Author:      Alex Roussinov
+//  Created:     2025-12-23
+//  Updated:     2025-12-23
+//
+//  //  This file is dual licensed under the MIT and Apache 2.0 licenses.
+//  Please see the root level LICENSE-MIT and LICENSE-APACHE files for details.
+// =============================================================================
+
+pub mod nlp;
+pub mod voice;
+pub mod api;
