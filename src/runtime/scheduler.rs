7ae4a607c312bfc97000f41615334b71144a2368
GD2BK1NG <147461464+gd2bk1ng@users.noreply.github.com>
2025-12-24 03:48:57 +0000

    Update scheduler.rs
    Task scheduler managing concurrency and timing.

--- src/runtime/scheduler.rs
+++ src/runtime/scheduler.rs
@@ -1,4 +1,15 @@
-/// Task scheduler managing concurrency and timing.
+// =============================================================================
+//  Astra Executor Runtime (AER)
+//  File: executor.rs
+//
+//  Description: Core scheduler for Astra programs, managing concurrency and timing.
+//  Author:      Alex Roussinov
+//  Created:     2025-12-22
+//  Updated:     2025-12-23
+//
+//  This file is dual licensed under the MIT and Apache 2.0 licenses.
+//  Please see the root level LICENSE-MIT and LICENSE-APACHE files for details.
+// =============================================================================
 
 pub struct Scheduler;
 
