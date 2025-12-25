14af8aeafb0789baa366857b9310c91c762a2ff7
GD2BK1NG <147461464+gd2bk1ng@users.noreply.github.com>
2025-12-25 09:58:50 +0000

    Update mod.rs
    pub mod extended_ontology

--- src/knowledge/mod.rs
+++ src/knowledge/mod.rs
@@ -6,7 +6,7 @@
 //
 //  Author:      Alex Roussinov
 //  Created:     2025-12-23
-//  Updated:     2025-12-24
+//  Updated:     2025-12-26
 //
 //  This file is dual licensed under the MIT and Apache 2.0 licenses.
 //  Please see the root level LICENSE-MIT and LICENSE-APACHE files for details.
@@ -16,6 +16,8 @@ mod tests;
 
 pub mod query;
 pub mod query_executor;
+
+pub mod extended_ontology;
 
 pub mod ontology;
 pub mod reasoner;
